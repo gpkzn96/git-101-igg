b7474b014d9772db7d8f8cda27a13d6d973a435e
Got <phgof.rock@gmail.com>
2020-09-16 15:45:07 +0700

    Create sync&Async.js

---
+++ javascriptest/sync&Async.js
@@ -0,0 +1,13 @@
+const { rejects } = require("assert")
+const { resolve } = require("dns")
+
+console.log("1")
+console.log("2")
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(console.log("3"))
+    }, 1000);
+}).then(() => {
+    console.log("4")
+    console.log("5")
+})
