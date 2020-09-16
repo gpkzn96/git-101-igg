5f080e435a22bed3f066b0950659d72cff3bf863
Got <phgof.rock@gmail.com>
2020-09-16 14:58:14 +0700

    quiz2
    q2loopmapfilter

---
+++ javascriptest/Quiz2.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 20; i += 1) {
+    console.log(i)
+}
+for (let i = 1; i <= 20; i++) {
+    console.log(i % 2 === 0 ? i : "")
+}
+let x1 = [-1, 2, 3, 4, 5, 6, 7, 0].map(el => el * 2)
+console.log(x1)
+let x2 = [-1, 2, 3, 4, 5, 6, 7, 0].filter(el => el / 2 === 0)
+console.log(x2)
