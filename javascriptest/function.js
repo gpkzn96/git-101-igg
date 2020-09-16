8fecc71a6673bd27a18fbb6b9eae5f5058c9a7c0
Got <phgof.rock@gmail.com>
2020-09-16 14:05:42 +0700

    function js
    fjs

---
+++ javascriptest/function.js
@@ -0,0 +1,11 @@
+function mul(a, b) {
+    return a * b
+}
+const add = (a, b) => a + b
+const add2 = (a, b) => {
+    this.user = 'JOke'
+    return a + b
+}
+console.log(this.user)
+
+console.log(add(9, 6))
