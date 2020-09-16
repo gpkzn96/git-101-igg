b9a0e386a2120b3c56486dc378f5200c61777016
Got <phgof.rock@gmail.com>
2020-09-16 15:00:20 +0700

    Update Quiz2.js
    mod2

--- javascriptest/Quiz2.js
+++ javascriptest/Quiz2.js
@@ -6,5 +6,5 @@ for (let i = 1; i <= 20; i++) {
 }
 let x1 = [-1, 2, 3, 4, 5, 6, 7, 0].map(el => el * 2)
 console.log(x1)
-let x2 = [-1, 2, 3, 4, 5, 6, 7, 0].filter(el => el / 2 === 0)
+let x2 = [-1, 2, 3, 4, 5, 6, 7, 0].filter(el => el % 2 === 0)
 console.log(x2)
